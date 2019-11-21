9e1d77cfb6169446e1d08362cf3775e7e1606244
Wenceslao <wenceslaomateos@gmail.com>
2019-11-21 16:30:52 -0300

    Coarrays funcionando con limite de operaciones

--- RBGS.f90
+++ RBGS.f90
@@ -1,6 +1,127 @@
 module RBGS
+    use SELs
     implicit none
     
 contains
-    
+
+    function RBGS1D(d, ld, rd, term_ind, xini, tol)
+        implicit none
+        real(8), dimension(:), intent(in) :: d, ld, rd, term_ind, xini
+        real(8), intent(in) :: tol
+        real(8), dimension(size(term_ind)) :: RBGS1D
+        real(8), dimension(:), codimension[:], allocatable :: res, xant
+        real(8) e1
+        integer(4) i, cont, orden, inicio, fin, tam_divisiones, im_act, arranca, termina, cota
+        
+        !definimos los inicios y finales del coarray correspondiente al problema real
+        orden = size(xini)
+        tam_divisiones = ceiling(real(orden) / real(num_images()))
+        inicio = tam_divisiones * this_image() - tam_divisiones + 1
+        fin = this_image() * tam_divisiones
+        allocate(res(tam_divisiones)[*])
+        if (this_image() < num_images()) then
+            res = xini(inicio:fin)
+        else
+            res(:ubound(xini,1)-inicio) = xini(inicio:)
+        end if
+        e1 = tol + 1
+        cont = 0
+        cota = orden - tam_divisiones * (num_images()-1)
+        !cambiar la cota de iteraciones
+        do while (cont < 36)
+            xant = res
+
+            !calculamos los rojos (impares)
+            
+            !calculo de los nodos del principio
+            if (this_image() == 1) then
+                !nodo primero de todos
+                res(1) = (term_ind(1) - rd(1) * res(2)) / d(1)
+                arranca = 3
+            else
+                !nodos al principio de cada imagen, que no son el primero
+                if (mod(inicio, 2) == 1) then
+                    !nodo impar, osea rojo al principio, y calculo con la imagen anterior
+                    res(1) = (term_ind(inicio) - ld(inicio) * res(tam_divisiones)[this_image()-1] - rd(inicio) * res(2)) / d(inicio)
+                    arranca = 3
+                else
+                    !nodo par al principio, asi que vamos al siguiente primero rojo
+                    arranca = 2
+                end if
+            end if
+
+            !Calculo de nodos del final
+            if (this_image() == num_images()) then
+                !calculo del nodo del final de todos
+                if (mod(fin, 2) == 1) then
+                    !final rojo
+                    res(cota) = (term_ind(orden) - ld(orden) * res(cota-1)) / d(orden)
+                end if
+                !sea o no el final un nodo rojo, lo salteamos, por que si es, ya lo calculamos, y si no es, no lo usamos
+                termina = cota-1
+            else
+                if (mod(fin, 2) == 1) then
+                    !nodo al final es rojo
+                    res(tam_divisiones) = (term_ind(fin) - ld(fin) * res(tam_divisiones-1) &
+                        - rd(fin) * res(1)[this_image()+1]) / d(fin)
+                end if
+                !si el nodo al final no es rojo lo salteamos, y si es, ya esta calculado
+                termina = tam_divisiones - 1
+            end if
+
+            !calculo de los nodos del medio
+            do i = arranca, termina, 2
+                res(i) = (term_ind(i+inicio) - ld(i+inicio) * res(i-1) - rd(i+inicio) * res(i+1)) / d(i+inicio)
+            end do
+
+            sync all
+
+            !calculamos los negros (pares)
+
+            !calculo de los nodos del principio (nunca va a ser negro el primero del vector original)
+            if (mod(inicio, 2) == 0) then
+                res(1) = (term_ind(inicio) - ld(inicio) * res(tam_divisiones)[this_image()-1] - rd(inicio) * res(2)) / d(inicio)
+                arranca = 3
+            else
+                arranca = 2
+            end if
+
+            !calculo de los nodos del final del todo
+            if (this_image() == num_images()) then
+                !estoy en la ultima imagen
+                if (mod(fin, 2) == 0) then
+                    !final negro
+                    res(cota) = (term_ind(orden) - ld(orden) * res(cota-1)) / d(orden)
+                end if
+                !si el final es negro, ya lo calcule, y si es rojo, no preciso calcularlo
+                termina = cota-1
+            else
+                if (mod(fin, 2) == 0) then
+                    !el ultimo es negro y lo calcculo
+                    res(tam_divisiones) = (term_ind(fin) - ld(fin) * res(tam_divisiones-1) &
+                        - rd(fin) * res(1)[this_image()+1]) / d(fin)
+                end if
+                !si es negro, ya lo calcule y lo evito, sino, no lo toco
+                termina = tam_divisiones - 1
+            end if
+
+            !calculo de los nodos del medio
+            do i = arranca, termina, 2
+                res(i) = (term_ind(i+inicio) - ld(i+inicio) * res(i-1) - rd(i+inicio) * res(i+1)) / d(i+inicio)
+            end do
+            sync all
+
+            e1 = errorAbsolutoV(res, xant, vNormaM)
+            cont = cont + 1
+        end do
+        !write(*, *) "Iteraciones: ", cont
+        sync all
+        if (this_image() == 1) then
+            do i = 1, num_images() - 1 
+                RBGS1D((i-1)*tam_divisiones+1:i*tam_divisiones) = res(:)[i]
+            end do
+            RBGS1D(1+tam_divisiones*(num_images()-1):) = res(:ubound(RBGS1D,1)-1+tam_divisiones*(num_images()-1))[num_images()]
+        end if
+    end function RBGS1D
+
 end module RBGS

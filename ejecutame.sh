fe208ad5d2b9e22953ba346c47f5a0c535e63014
Wenceslao <wenceslaomateos@gmail.com>
2019-11-21 15:51:40 -0300

    Cambiado script para ejecucion adaptable
    Co-authored-by: Bravlin <bravlinleonel@gmail.com>

--- ejecutame.sh
+++ ejecutame.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+hilos=$(nproc)
 gfortran -g -c -fcoarray=lib RBGS.f90 -lcaf_mpi 
 gfortran -g -c SELs.f90
 gfortran -g -c arreglos.f90
 gfortran -g arreglos.o SELs.o RBGS.o -fcoarray=lib main.f90 -lcaf_mpi -o main
-cafrun -np $1 ./main
+cafrun -np $hilos ./main

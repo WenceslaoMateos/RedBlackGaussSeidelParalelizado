64dfe134074b9fc48188d68344baee59b2e0b7b6
Wenceslao <wenceslaomateos@gmail.com>
2019-11-21 11:28:05 -0300

    ejecutame acomodad para concurrencia

---
+++ ejecutame.sh
@@ -0,0 +1,5 @@
+gfortran -g -c -fcoarray=lib RBGS.f90 -lcaf_mpi 
+gfortran -g -c SELs.f90
+gfortran -g -c arreglos.f90
+gfortran -g arreglos.o SELs.o RBGS.o -fcoarray=lib main.f90 -lcaf_mpi -o main
+cafrun -np $1 ./main

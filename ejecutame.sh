801c56a85f37e8ac627d6331816c1dd1f145b3cf
Bravlin <bravlinleonel@gmail.com>
2019-11-22 15:15:44 -0300

    main2 por hacer andar

--- ejecutame.sh
+++ ejecutame.sh
@@ -3,5 +3,5 @@ hilos=$(nproc)
 gfortran -g -c -fcoarray=lib RBGS.f90 -lcaf_mpi 
 gfortran -g -c SELs.f90
 gfortran -g -c arreglos.f90
-gfortran -g arreglos.o SELs.o RBGS.o -fcoarray=lib main.f90 -lcaf_mpi -o main
-cafrun -np $hilos ./main
+gfortran -g arreglos.o SELs.o RBGS.o -fcoarray=lib $1 -lcaf_mpi -o $(basename $1 .f90)
+cafrun -np $hilos ./$(basename $1 .f90)

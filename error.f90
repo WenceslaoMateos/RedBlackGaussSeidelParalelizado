277a0d142cb47322b92afc20bc239e5313d92d61
Acytra <marianof.06@gmail.com>
2019-11-23 18:18:34 -0300

    error thomas

--- error.f90
+++ error.f90
@@ -5,18 +5,20 @@ real function normaVectorResiduoConVector(ld, d, rd, vectorResultado, b, n)
     real vectorR(n)
     
     vectorR = 0
-  
-    do i=1, n
-      vectorR(i) = ld(i)*vectorResultado(i) + d(i)*vectorResultado(i) + rd(i)*vectorResultado(i) - b(i)
+    
+    vectorR(i) = d(1)*vectorResultado(1) + rd(2)*vectorResultado(2) - b(1)
+    do i=2, n-1
+      vectorR(i) = ld(i)*vectorResultado(i-1) + d(i)*vectorResultado(i) + rd(i)*vectorResultado(i+1) - b(i)
     enddo
+    vectorR(i) = ld(n-1)*vectorResultado(n-1) + d(n)*vectorResultado(n) - b(n)
     
     normaVectorResiduoConVector = normaVector(vectorR, n)
     
-  endfunction
+  end function
 
   real function normaVector(vector, n)
     integer n
     real vector(n)
     
     normaVector = maxval(abs(vector))
-  endfunction
+  end function

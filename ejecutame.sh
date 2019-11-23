86e3712e36faa0749e2b358dbe288e1abadee4ea
Wenceslao <wenceslaomateos@gmail.com>
2019-11-23 20:32:49 -0300

    Eliminado de librerias inutiles

--- ejecutame.sh
+++ ejecutame.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 hilos=$(nproc)
 gfortran -g -c -fcoarray=lib RBGS.f90 -lcaf_mpi 
+gfortran -g -c generadorDeCasos.f90
 gfortran -g -c SELs.f90
-gfortran -g -c moduloArchivos.f90
-gfortran -g -c arreglos.f90
-gfortran -g moduloArchivos.o arreglos.o SELs.o RBGS.o -fcoarray=lib $1 -lcaf_mpi -o $(basename $1 .f90)
+gfortran -g generadorDeCasos.o SELs.o RBGS.o -fcoarray=lib $1 -lcaf_mpi -o $(basename $1 .f90)
 cafrun -np $hilos ./$(basename $1 .f90)

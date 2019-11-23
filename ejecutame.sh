20df9e0b009f175b5fe27b1b99c85e09f778c4d8
Wenceslao <wenceslaomateos@gmail.com>
2019-11-23 17:16:06 -0300

    cambiado el ejecutame para carga de archivos

--- ejecutame.sh
+++ ejecutame.sh
@@ -2,6 +2,7 @@
 hilos=$(nproc)
 gfortran -g -c -fcoarray=lib RBGS.f90 -lcaf_mpi 
 gfortran -g -c SELs.f90
+gfortran -g -c moduloArchivos.f90
 gfortran -g -c arreglos.f90
-gfortran -g arreglos.o SELs.o RBGS.o -fcoarray=lib $1 -lcaf_mpi -o $(basename $1 .f90)
+gfortran -g moduloArchivos.o arreglos.o SELs.o RBGS.o -fcoarray=lib $1 -lcaf_mpi -o $(basename $1 .f90)
 cafrun -np $hilos ./$(basename $1 .f90)
